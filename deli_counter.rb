9b1fe362e1406762337916998c4a1926163bd289
Luis Rodriguez <zacha@tutanota.com>
2018-04-04 23:07:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    array.each_with_index do |customer, index|
+      string += " #{index + 1}. #{customer}"
+    end
+  end
+end
